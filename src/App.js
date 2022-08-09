471efce17a5395ccda1f983a40528cd4a423aaf3
Nick Don <nick.don.1102@gmail.com>
2022-08-09 20:18:41 +1000

    create to-do-list

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useRef } from "react";
+import TodoList from "./TodoList";
 
 function App() {
+  const [todos, setTodos] = useState([])
+  const todoNameRef = useRef()
+
+  function  handledAddTodo(e) {
+    const name = todoNameRef.current.value
+    if (name === '') return
+    setTodos(prevTodos =>{
+      return [...prevTodos, {id:1, name:name, complete:false}]
+    })
+    todoNameRef.current.value = null
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+    <TodoList todos={todos} />
+    <input ref={todoNameRef} type="text" />
+    <button onClick={handledAddTodo}>Add Todo </button>
+    <button>Clear Completed </button>
+    <div>0 Left to do</div>
+    </>
+  )
 }
 
 export default App;
